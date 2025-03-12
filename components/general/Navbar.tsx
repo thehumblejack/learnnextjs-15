66fb473882458486a56f1d7631021a2e80f79c85
macbook pro <thehumblejack>
2025-03-12 03:55:40 +0100

    sev

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -6,7 +6,6 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { buttonVariants } from "../ui/button";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Navbar() {
